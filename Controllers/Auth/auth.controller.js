0800aacfe7f45966e74a389d9c3f22df9e08c6f6
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-31 15:35:19 +0530

    added register endpoint

--- Controllers/Auth/auth.controller.js
+++ Controllers/Auth/auth.controller.js
@@ -6,16 +6,58 @@ const Joi = require('../../Config/validater.config');
 router.post('/register' , async (req ,res) => {
 
     const validator = Joi.validate(req.body, {
+        fullname: Joi.string().required(),
+        phone: Joi.string().required(),
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required(),
-        first_name: Joi.string().required(),
-        last_name: Joi.string().required(),
-        phone: Joi.string().required(),
-        role: Joi.string().required(),
     });
 
-    let data = await validator.validateAsync(req.body , {abortEarly: false});
-    
+    try{
+        let data = await validator.validateAsync(req.body , {abortEarly: false});
+        // go ahead
+
+        // check if user exists by email or phone
+        let checkUser = await db.users.findOne({
+            where: {
+                [db.Sequelize.Op.or]: [
+                    {email: data.email},
+                    {phone: data.phone}
+                ]
+            }
+        });
+
+        if(checkUser){
+            return res.status(400).json({error: 'User already exists'});
+        }
+
+        // create user
+        let user = await db.users.create({
+            fullname: data.fullname,
+            phone: data.phone,
+            email: data.email,
+            password: data.password,
+        });
+
+        // generate token
+        const token = null
+
+        // save token
+        await db.access_token.create({
+            user_id: user.id,
+            token: token
+        });
+
+        // return user
+        return res.status(200).json({
+            user: user,
+            token: token
+        });
+
+
+    }catch(err){
+        return res.status(400).send(err.details);
+    }
+
 
 });
 
