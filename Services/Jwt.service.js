1488a90f5eb150d96f03052f8301abc3a27f25ef
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-31 15:36:32 +0530

    added jwt service

---
+++ Services/Jwt.service.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+const Uuid = require('uuid');
+
+module.exports = {
+
+    generateToken(email){
+        return jwt.sign( {email : email} , process.env.JWT_SECRET , {expiresIn: '30d'});
+    },
+
+};
