55896a7880cb20a983e12a5af77ac28d953c7415
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-31 14:16:13 +0530

    added start script command

--- Routes/api.js
+++ Routes/api.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const AuthRequired = require('../Middlewares/AuthRequired');
-
-const authController = require('../Controllers/Auth/auth.controller');
-const appController = require( '../Controllers/app.controller');
-const workspaceController = require('../Controllers/workspace.controller');
 
 router.get('/', (req, res) => {
     res.json('SpringDB API');
@@ -13,8 +8,8 @@ router.get('/', (req, res) => {
 // add middleware to jobController
 // router.use('/job' , AuthRequired('User') ,  JobController);
 
-router.use('/auth' , authController);
-router.use('/app' , AuthRequired('user') , appController);
-router.use('/workspace' , AuthRequired('user') , workspaceController);
+// router.use('/auth' , authController);
+// router.use('/app' , AuthRequired('user') , appController);
+// router.use('/workspace' , AuthRequired('user') , workspaceController);
 
 module.exports = router;
