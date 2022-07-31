a094fc8bf631c226719232266eedfd8a96053b15
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-31 14:19:43 +0530

    added user model

---
+++ Models/User.model.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("users", {
+
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        fullname: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        },
+
+        password: {
+            type: Sequelize.STRING,
+            allowNull : false
+        },
+
+    });
+    return User;
+};
--- Routes/api.js
+++ Routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('SpringDB API');
+    res.json('UNI-CHAT API');
 });
 
 // add middleware to jobController
