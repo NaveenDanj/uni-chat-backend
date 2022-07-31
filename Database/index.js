d3599e997139c1cd37234daffc4cc9f3d32480b4
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-31 14:14:25 +0530

    added db config and index files

---
+++ Database/index.js
@@ -0,0 +1,27 @@
+const dbConfig = require("../config/db.config");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.users = require("../Models/User.model")(sequelize, Sequelize);
+db.access_token = require("../Models/AccessToken.model")(sequelize, Sequelize);
+db.workspaces = require("../Models/Workspace.model")(sequelize, Sequelize);
+db.workspace_users = require("../Models/WorkspaceUser.model")(sequelize, Sequelize);
+db.instance = require("../Models/Instance.model")(sequelize, Sequelize);
+db.backup = require("../Models/Backup.model")(sequelize, Sequelize);
+db.app = require("../Models/App.model")(sequelize, Sequelize);
+db.app_users = require("../Models/AppUser.model")(sequelize, Sequelize);
+db.error_log = require("../Models/ErrorLog.model")(sequelize, Sequelize);
+module.exports = db;
