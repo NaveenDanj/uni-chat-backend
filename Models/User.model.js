c8bc13b1d640ebd34c56d5f6dfb9c6313e8eb0a0
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-31 14:28:49 +0530

    edited user model

--- Models/User.model.js
+++ Models/User.model.js
@@ -12,6 +12,30 @@ module.exports = (sequelize, Sequelize) => {
             allowNull: false
         },
 
+        designation: {
+            type: Sequelize.STRING,
+            validate: {
+                len: [0, 50]
+            },
+            allowNull: false
+        },
+
+        about : {
+            type: Sequelize.STRING,
+            validate: {
+                len: [0, 255]
+            },
+            allowNull: true
+        },
+
+        location : {
+            type: Sequelize.STRING,
+            validate: {
+                len: [0, 100]
+            },
+            allowNull: true
+        },
+
         email: {
             type: Sequelize.STRING,
             allowNull: false,
