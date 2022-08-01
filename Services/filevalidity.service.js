65894a1ddbf5c8fd8f975be57090fa1ee8bee9a4
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-08-01 12:40:13 +0530

    added file validity service

---
+++ Services/filevalidity.service.js
@@ -0,0 +1,39 @@
+const Uuid = require('uuid');
+
+module.exports = {
+
+    checkAllowedExtension(file , occassion){
+
+        let allowedExtensions = [];
+
+        switch(occassion){
+            case 'profile-picture':
+                allowedExtensions = ['jpg' , 'jpeg' , 'png'];
+                break;
+            case 'cover-picture':
+                allowedExtensions = ['jpg' , 'jpeg' , 'png'];
+                break;
+            case 'post-image':
+                allowedExtensions = ['jpg' , 'jpeg' , 'png'];
+                break;
+            case 'post-video':
+                allowedExtensions = ['mp4' , 'mkv' , 'avi'];
+                break;
+            default:
+                allowedExtensions = ['jpg' , 'jpeg' , 'png'];
+                break;
+        }
+
+        let fileExtension = file.name.split('.').pop();
+
+        if(allowedExtensions.includes(fileExtension)){
+            return true;
+        }else{
+            return false;
+        }
+
+
+    }
+
+
+};
