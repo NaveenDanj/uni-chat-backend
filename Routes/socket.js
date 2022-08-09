77a60004381b463e102ca88b8146296a2fefe707
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-08-09 15:32:39 +0530

    added small changes

--- Routes/socket.js
+++ Routes/socket.js
@@ -1,21 +1,9 @@
-// const onConnection = (socket) => {
-//     // socket.on("order:create", createOrder);
-//     // socket.on("order:read", readOrder);
-
-//     // socket.on("user:update-password", updatePassword);
-//     console.log('client connected!');
-// }
-
-// module.exports = onConnection;
-
-
 module.exports = function(io){
 
     const {joinToMainChannel , sendToMainChannel} = require('../SocketEvent/MainChat')(io);
 
 
     const onConnection = function(socket){
-        console.log('client connected with socket id => ' , socket.id);
         socket.on("chennel:main:sendMessage" , sendToMainChannel);
         socket.on("channel:main:join" , joinToMainChannel);
     }
--- SocketEvent/MainChat.js
+++ SocketEvent/MainChat.js
@@ -11,20 +11,17 @@ module.exports = function(io){
 
         socket.emit('channel:main:meJoined' , {
             message: 'joined to main channel!',
-            data : "this is from the web rtc server"
         });
         
         // let other users in the main channel know that a new user has joined
         socket.broadcast.to('main-lobby').emit('channel:main:userJoined' , {
             message: 'a new user has joined the main channel!',
-            data : "this is from the web rtc server"
         });
 
         // let other users know that the user has left the main channel
         socket.on('disconnect' , () => {
             socket.broadcast.to('main-lobby').emit('channel:main:userLeft' , {
                 message: 'a user has left the main channel!',
-                data : "this is from the web rtc server"
             });
         } );
 
@@ -32,7 +29,10 @@ module.exports = function(io){
 
     const sendToMainChannel = function(payload){
         const socket = this;
-        socket.broadcast.to('main-lobby').emit('channel:main:receiveMessage' , payload);
+        let _payload = payload;
+        _payload.id = Date.now()+'' + socket.id;
+
+        socket.broadcast.to('main-lobby').emit('channel:main:receiveMessage' , _payload);
     }
 
     return {
