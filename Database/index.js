651a9e2bafd4f59619ab1727b5479008dd2f876a
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-08-16 21:14:50 +0530

    fixed env undefined issue

--- Database/index.js
+++ Database/index.js
@@ -1,5 +1,7 @@
 const dbConfig = require("../Config/db.config");
 const Sequelize = require("sequelize");
+
+
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
